0d3f7bc2c0fd7a02491174eba0805f41987bb835
anasommer <68922341+anasommer@users.noreply.github.com>
2025-02-19 21:09:38 +0100

    removed router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
