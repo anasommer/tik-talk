517d1e98cdf35469b3e7e9ecbc4dfca8f1c56cdf
Anastassia Sommer <anastassia.sommer@gmail.com>
2025-02-20 20:40:57 +0100

    http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { ProfileService } from './data/services/profile.service';
+import { Component, inject } from '@angular/core';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCardComponent],
+  imports: [ProfileCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'tik-talk';
+  profileService = inject(ProfileService);
+  profiles: any = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
