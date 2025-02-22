b3d13a632af65c7e3559ea614b4ca882d1096a73
anasommer <68922341+anasommer@users.noreply.github.com>
2025-02-22 09:06:00 +0100

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
-import { ProfileService } from './data/services/profile.service';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 import { CommonModule } from '@angular/common';
-import { Profile } from './data/interfaces/profile.interface';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCardComponent, CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((val) => {
-      this.profiles = val;
-    });
-  }
-}
+export class AppComponent {}
