7ad4a602ee1ffcf91277f7b4417262ab04f096c9
Anastassia Sommer <anastassia.sommer@gmail.com>
2025-02-23 10:19:14 +0100

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
